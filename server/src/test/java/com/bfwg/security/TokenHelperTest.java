7239df37f4c378a0b1d527bf3914fc256961ac1d
restnek <restnek@gmail.com>
2021-04-17 02:31:55 +0300

    Update TokenHelperTest, get rid of Joda-Time

--- server/src/test/java/com/bfwg/security/TokenHelperTest.java
+++ server/src/test/java/com/bfwg/security/TokenHelperTest.java
@@ -1,29 +1,49 @@
 package com.bfwg.security;
 
-import io.jsonwebtoken.Jwts;
-import org.joda.time.DateTimeUtils;
+import java.time.Clock;
+import java.time.Duration;
+
+import com.bfwg.config.JwtProperties;
+import com.bfwg.service.impl.UserDetailsLoaderService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
 public class TokenHelperTest {
     private TokenHelper tokenHelper;
+    private UserDetailsService userDetailsService;
+    private JwtProperties jwtProperties;
+    private Clock clock = Clock.systemDefaultZone();
 
     @BeforeEach
     public void init() {
-        tokenHelper = new TokenHelper();
-        DateTimeUtils.setCurrentMillisFixed(20L);
-        ReflectionTestUtils.setField(tokenHelper, "EXPIRES_IN", 1);
-        ReflectionTestUtils.setField(tokenHelper, "SECRET", "mySecret");
+        userDetailsService = mock(UserDetailsLoaderService.class);
+
+        jwtProperties = new JwtProperties();
+        jwtProperties.setIssuer("test");
+        jwtProperties.setExpiration(10);
+
+        tokenHelper = new TokenHelper(userDetailsService, jwtProperties, clock);
     }
 
     @Test
-    public void testGenerateTokenExpired() {
-        // expected ExpiredJwtException
-        String token = tokenHelper.generateToken("fanjin");
-        Jwts.parser()
-                .setSigningKey("mySecret")
-                .parseClaimsJws(token)
-                .getBody();
+    public void testCanTokenBeRefreshedIfNotExpire() {
+        String token = tokenHelper.generateToken("restnek");
+        assertTrue(tokenHelper.canTokenBeRefreshed(token));
+    }
+
+    @Test
+    public void testCanTokenBeRefreshedIfExpire() {
+        String token = tokenHelper.generateToken("restnek");
+
+        tokenHelper.setClock(Clock.offset(
+                clock,
+                Duration.ofSeconds(jwtProperties.getExpiration())));
+
+        assertFalse(tokenHelper.canTokenBeRefreshed(token));
     }
 }
